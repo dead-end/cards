9939fab341ace9bb2bbf3a56ab7becb02af7a542
dead end <dead.end@inbox.ru>
2021-02-22 10:22:03 +0100

    update

--- docs/modules/pool-list.js
+++ docs/modules/pool-list.js
@@ -1,5 +1,5 @@
 import Persist from "./persist.js";
-import { fmtDate, arrValueIs, arrPercentage, elemRemoveById } from "./utils.js";
+import { fmtDate, arrPercentage, elemRemoveById } from "./utils.js";
 
 /******************************************************************************
  * The class implements a list with files that contain the question pools.
@@ -10,10 +10,7 @@ export default class PoolList {
    * The constructor does the binding for the callback functions.
    ***************************************************************************/
   constructor(dispatcher) {
-    this._onSelect = this._onSelect.bind(this);
-    this._onButton = this._onButton.bind(this);
     this.dispatcher = dispatcher;
-    this.selected = {};
   }
 
   /****************************************************************************
@@ -31,14 +28,6 @@ export default class PoolList {
       entry.querySelector(".tpl-modified").innerText = fmtDate(
         persist.lastmodified
       );
-
-      //
-      // disable the start button if all questions are learned.
-      //
-      entry.querySelector(".tpl-start").disabled = arrValueIs(
-        persist.answer,
-        3
-      );
     } else {
       entry.querySelector(".tpl-status").innerText = "";
       entry.querySelector(".tpl-modified").innerText = "";
@@ -48,7 +37,7 @@ export default class PoolList {
   /****************************************************************************
    * The function creates the tables from the templates.
    ***************************************************************************/
-  _show() {
+  doShow() {
     let temp = document.getElementById("tmpl-pool-list");
     let tplEnty = document.getElementById("tpl-entry");
 
@@ -63,18 +52,14 @@ export default class PoolList {
       this._updatePersist(entry, this.files[i].file);
 
       //
-      // Add the start button
+      // Add the show button
       //
-      let button = entry.querySelector(".tpl-start");
+      let button = entry.querySelector(".tpl-show");
       button.setAttribute("data-file-idx", i);
-      button.addEventListener("click", this._onButton);
-
-      //
-      // Add the select box
-      //
-      let select = entry.querySelector(".tpl-set");
-      select.setAttribute("data-file-idx", i);
-      select.addEventListener("change", this._onSelect);
+      button.addEventListener("click", (e) => {
+        let idx = e.target.getAttribute("data-file-idx");
+        this.dispatcher.onFileSelected(this.files[idx]);
+      });
 
       //
       // Add the result to the dom and the add the id
@@ -87,76 +72,18 @@ export default class PoolList {
   }
 
   /****************************************************************************
-   * The callback function gets the question pool id from the pressed button
-   * and fires an event.
-   ***************************************************************************/
-  _onButton(e) {
-    let idx = e.target.getAttribute("data-file-idx");
-    this.selected = this.files[idx];
-    this.dispatcher.onFileSelected(this.files[idx]);
-  }
-
-  /****************************************************************************
-   * The callback function which is called to set all ansers to a given value.
-   ***************************************************************************/
-  _onSelect(e) {
-    //
-    // We are only intereseted in a positive index.
-    //
-    if (e.target.selectedIndex) {
-      let idx = e.target.getAttribute("data-file-idx");
-      let id = this.files[idx].file;
-
-      let val = e.target.selectedIndex - 1;
-      Persist.setAll(id, val);
-
-      //
-      // Set the index to 0 to restore the orignal state.
-      //
-      e.target.selectedIndex = 0;
-
-      //
-      // Update this component with the new values
-      //
-      let entry = document.getElementById("tplid-" + id);
-      this._updatePersist(entry, id);
-    }
-  }
-
-  /****************************************************************************
    * The function is a callback function, which is called after the registry
    * was loaded
    ***************************************************************************/
   onLoadedRegistry(files) {
     this.files = files;
-    this._show();
+    this.doShow();
   }
 
   /****************************************************************************
-   * Callback function for the stop event.
+   * The function removes the content from the dom.
    ***************************************************************************/
   doHide() {
-    //
-    // On start remove the table
-    //
     elemRemoveById("tpl-table");
-  }
-
-  /****************************************************************************
-   * Callback function for the stop event.
-   ***************************************************************************/
-  doShow() {
-    //
-    // On stop show the table.
-    //
-    this._show();
-
-    //
-    // Update the result
-    //
-    let entry = document.getElementById("tplid-" + this.selected.file);
-    this._updatePersist(entry, this.selected.file);
-
-    this.selected = {};
   }
 }
